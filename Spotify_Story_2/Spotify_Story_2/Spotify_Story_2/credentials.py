3a34508b39eff659dfad2028de27ab4101e87ccc
VGandrade <148589849+VGandrade@users.noreply.github.com>
2024-11-22 16:41:06 -0500

    deleted old version

--- Spotify_Story_2/Spotify_Story_2/Spotify_Story_2/credentials.py
+++
@@ -1,9 +0,0 @@
-# from decouple import config
-
-import os
-config = lambda x: os.getenv(x)
-
-
-CLIENT_ID = config('CLIENT_ID')
-CLIENT_SECRET = config('CLIENT_SECRET')
-REDIRECT_URI = config('REDIRECT_URI')
--- Story_2/Spotify_Story_2/credentials.ps1
+++ Story_2/Spotify_Story_2/credentials.ps1
@@ -1,4 +1,5 @@
-$env:CLIENT_ID="0fdc7efc8ee24f0d836dff40a3491799"
+
+']'$env:CLIENT_ID="0fdc7efc8ee24f0d836dff40a3491799"
 $env:CLIENT_SECRET="969addd1ce50418da9a035d3d388510f"
 $env:REDIRECT_URI="http://127.0.0.1:8000/spotify/callback"
 
